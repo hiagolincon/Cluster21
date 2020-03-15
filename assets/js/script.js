2dc05157351cab5f5046ba5e11bec6058627cbd5
João Artur <contato@joaoartur.com>
2020-03-14 22:08:53 -0300

    slides

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,7 +81,7 @@ $(document).ready(function () {
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
